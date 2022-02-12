e3bb543a20d74afdf991c98e8ff4952d4d1585e2
xiabin <272676785@qq.com>
2022-02-12 17:21:54 +0800

    aa

---
+++ src/main/java/com/binx/cms/service/ISysUserService.java
@@ -0,0 +1,16 @@
+package com.binx.cms.service;
+
+import com.binx.cms.auto.SysUser;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author X .
+ * @since 2022-01-15
+ */
+public interface ISysUserService extends IService<SysUser> {
+
+}
